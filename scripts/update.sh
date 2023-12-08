b4b3d15bdee7426d842e9f9c4a4fc39492af31a3
rtx <123107610+rtx-vm@users.noreply.github.com>
2023-12-08 03:03:38 +0000

    improve update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,14 +6,23 @@ set -euxo pipefail
 export RTX_NODE_MIRROR_URL="https://nodejs.org/dist/"
 
 fetch() {
+  lines=$(wc -l < "versions/$1")
   docker run jdxcode/rtx -y ls-remote "$1" > "versions/$1" || true
+  new_lines=$(wc -l < "versions/$1")
+  if [ "$lines" == "$new_lines" ]; then
+    echo "No new versions for $1"
+  elif [ ! "$new_lines" -gt 1 ]; then
+    echo "No versions for $1"
+  else
+    echo "New versions for $1"
+    git add "versions/$1"
+  fi
 }
 
-docker run jdxcode/rtx plugins --all | env_parallel --env fetch fetch {}
+docker run jdxcode/rtx plugins --all | env_parallel -j4 --env fetch fetch {}
 
 git config --local user.email "123107610+rtx-vm@users.noreply.github.com"
 git config --local user.name "rtx"
-git add versions
 if [ "$DRY_RUN" == 0 ] && ! git diff-index --quiet HEAD; then
   git commit -m "Update release metadata"
   git push
