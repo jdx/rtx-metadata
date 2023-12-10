7fee8de260d9f655c527864fa9c60fbc61457248
Jeff Dickey <216188+jdx@users.noreply.github.com>
2023-12-09 18:14:24 -0600

    only set git config if not dry run

--- scripts/update.sh
+++ scripts/update.sh
@@ -24,9 +24,9 @@ fetch() {
 
 docker run jdxcode/rtx plugins --all | env_parallel -j4 --env fetch fetch {}
 
-git config --local user.email "123107610+rtx-vm@users.noreply.github.com"
-git config --local user.name "rtx"
 if [ "$DRY_RUN" == 0 ] && ! git diff-index --quiet HEAD; then
+  git config --local user.email "123107610+rtx-vm@users.noreply.github.com"
+  git config --local user.name "rtx"
   git commit -m "Update release metadata"
   git push
 fi
