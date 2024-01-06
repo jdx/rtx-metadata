2d06343bdde28b7413fe6948cfffe219d79fc598
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-01-06 16:16:54 -0600

    wip

--- src/worker.ts
+++ src/worker.ts
@@ -1,36 +1,21 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import { Request } from "@cloudflare/workers-types";
+import { Request, Queue, MessageBatch } from "@cloudflare/workers-types";
 import { Database } from "./database.types.js";
 
-type Env = {
-  SUPABASE_URL: string;
-  SUPABASE_KEY: string;
+type VersionRequest =
+  Database["public"]["Tables"]["version_requests"]["Insert"];
+type VersionRequestMessage = Omit<VersionRequest, "plugin_id"> & {
+  plugin: string;
 };
 
-class Worker {
+interface Env {
+  SUPABASE_URL: string;
+  SUPABASE_KEY: string;
+  MISE_VERSIONS_QUEUE: Queue<VersionRequestMessage>;
+}
+
+class DB {
   constructor(private supabase: SupabaseClient<Database>) {}
-
-  handle(request: Request) {
-    const plugin = request.url.split("/")[3];
-    if (!plugin) return new Response("Not found", { status: 404 });
-
-    const url = new URL(request.url);
-    url.hostname = "mise-versions-gh.jdx.dev";
-    const response = await fetch(url);
-    if (response.status !== 200) return response;
-
-    const pluginID = await this.getOrCreatePlugin(plugin);
-    await this.supabase.from("version_requests").insert({
-      plugin_id: pluginID,
-      ip: request.headers.get("cf-connecting-ip"),
-      user_agent: request.headers.get("user-agent"),
-      country: request.cf!.country,
-      region: request.cf!.region,
-      city: request.cf!.city,
-    });
-
-    return response;
-  }
 
   async getOrCreatePlugin(name: string) {
     const id = await this.getPluginID(name);
@@ -60,8 +45,46 @@ class Worker {
 
 export default {
   async fetch(request: Request, env: Env) {
+    const plugin = request.url.split("/")[3];
+    if (!plugin) return new Response("Not found", { status: 404 });
+
+    const url = new URL(request.url);
+    url.hostname = "mise-versions-gh.jdx.dev";
+    const response = await fetch(url);
+    if (response.status !== 200) return response;
+
+    await env.MISE_VERSIONS_QUEUE.send({
+      plugin,
+      ip: request.headers.get("cf-connecting-ip")!,
+      user_agent: request.headers.get("user-agent")!,
+      country: request.cf!.country as string,
+      region: request.cf!.region as string,
+      city: request.cf!.city as string,
+    });
+
+    return new Response(await response.text(), {
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  },
+
+  async queue(batch: MessageBatch<VersionRequestMessage>, env: Env) {
     const supabase = createClient<Database>(env.SUPABASE_URL, env.SUPABASE_KEY);
-    const worker = new Worker(supabase);
-    return worker.handle(request);
+    const db = new DB(supabase);
+
+    for (const { body: message } of batch.messages) {
+      const pluginID = await db.getOrCreatePlugin(message.plugin);
+      await supabase.from("version_requests").insert({
+        plugin_id: pluginID,
+        ip: message.ip,
+        user_agent: message.user_agent,
+        country: message.country,
+        region: message.region,
+        city: message.city,
+      });
+    }
   },
 };
