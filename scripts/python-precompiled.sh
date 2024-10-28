693b851f23f0397c60cef87cb3a0fb9ea97e62e1
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-10-28 00:30:22 +0000

    init

---
+++ scripts/python-precompiled.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -xeu #o pipefail
+
+releases=$(gh api graphql -f query="
+    query {
+      repository(owner: \"indygreg\", name: \"python-build-standalone\") {
+        releases(first: $1) {
+           nodes { name }
+         } 
+      }
+    }" --jq '.[].repository.releases.nodes.[].name')
+
+for release in $releases; do
+  assets=$(gh api graphql --paginate -f query="
+    query(\$endCursor: String) {
+      repository(owner: \"indygreg\", name: \"python-build-standalone\") {
+        release(tagName: \"$release\") {
+          releaseAssets(first: 100, after: \$endCursor) {
+            nodes { name }
+            pageInfo { hasNextPage, endCursor }
+          }
+        }
+      }
+    }" --jq '.[].repository.release.releaseAssets.nodes.[].name' | grep install_only | grep -ve '\.sha256$')
+  echo "$assets" >> docs/python-precompiled
+done
+
+sort -uV >docs/python-precompiled.tmp <docs/python-precompiled
+mv docs/python-precompiled.tmp docs/python-precompiled
+gzip -n9c docs/python-precompiled >docs/python-precompiled.gz
+git add docs/python-precompiled*
---
+++ scripts/update.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+source env_parallel.bash
+set -euxo pipefail
+
+export MISE_NODE_MIRROR_URL="https://nodejs.org/dist/"
+export MISE_USE_VERSIONS_HOST=0
+export MISE_LIST_ALL_VERSIONS=1
+
+fetch() {
+  case "$1" in
+    awscli-local) # TODO: remove this when it is working
+      echo "Skipping $1"
+      return
+      ;;
+    jfrog-cli|minio|tiny|teleport-ent|flyctl|flyway|vim|awscli|checkov|snyk|chromedriver|sui)
+      echo "Skipping $1"
+      return
+      ;;
+  esac
+  if ! docker run -e GITHUB_API_TOKEN -e MISE_USE_VERSIONS_HOST \
+      jdxcode/mise -y ls-remote "$1" > "docs/$1"; then
+    echo "Failed to fetch versions for $1"
+    return
+  fi
+  new_lines=$(wc -l < "docs/$1")
+  if [ ! "$new_lines" -gt 1 ]; then
+    echo "No versions for $1" >/dev/null
+  else
+    case "$1" in
+      rust)
+        if [ "$new_lines" -gt 10 ]; then
+          git add "docs/$1"
+        fi
+        ;;
+      java)
+        sort -V "docs/$1" -o "docs/$1"
+        git add "docs/$1"
+        ;;
+      vault|consul|nomad|terraform|packer|vagrant|boundary|protobuf)
+        sort -V "docs/$1" -o "docs/$1"
+        git add "docs/$1"
+        ;;
+      *)
+        git add "docs/$1"
+        ;;
+    esac
+  fi
+}
+
+docker run -e MISE_EXPERIMENTAL=1 jdxcode/mise registry | awk '{print $1}' | env_parallel -j4 --env fetch fetch {} || true
+
+if [ "$DRY_RUN" == 0 ] && ! git diff-index --cached --quiet HEAD; then
+  git diff --compact-summary --cached
+  git config --local user.email "123107610+rtx-vm@users.noreply.github.com"
+  git config --local user.name "rtx"
+  git commit -m "Update release metadata"
+  git push
+fi
